3f3a524fd44375ea1880459d83e2e33258467f5e
Jean-pierre14 <chiruzabisimwa@gmail.com>
2022-08-19 22:52:46 +0200

    Class extends

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-export default function App() {
-    return (
-
-        <>
-        <h3> Hello </h3>
-        </>
-    )
+import React from "react";
+class App extends React.Component{
+    render(){
+        return(
+            <>
+                <h3>Hello world</h3>
+            </>
+        )
+    }
 }
+export default App;
---
+++ src/Container.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Container extends React.Componet{
+    render(){
+        return(
+            <>
+                <h4>Container</h4>
+            </>
+        )
+    }
+}
+
+export default Container;
